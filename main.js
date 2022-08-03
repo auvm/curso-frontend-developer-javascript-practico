c674ded68738fa85cce6dece6cb8468118cdb4f9
auvm <angelurielvelascom@gmail.com>
2022-08-03 13:57:49 -0500

    add shopping cart functionality and compatibility with the menus

--- main.js
+++ main.js
@@ -5,14 +5,40 @@ const menuBurgerIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 
 
+const shoppingCartIcon = document.querySelector(".navbar-shopping-cart");
+const shoppingCart = document.querySelector(".product-detail");
+
 menuEmail.addEventListener("click", toggleDesktopMenu);
 
 menuBurgerIcon.addEventListener("click", toggleMobileMenu);
 
+shoppingCartIcon.addEventListener("click", toggleShoppingCart);
+
 function toggleDesktopMenu(){
+    let isShoppingCartOpen = !shoppingCart.classList.contains("inactive");
+    if(isShoppingCartOpen){
+        shoppingCart.classList.add("inactive");
+    }
     desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu(){
+    let isShoppingCartOpen = !shoppingCart.classList.contains("inactive");
+    if(isShoppingCartOpen){
+        shoppingCart.classList.add("inactive");
+    }
     mobileMenu.classList.toggle("inactive");
+}
+
+function toggleShoppingCart(){
+    let isMobileMenuOpen = !mobileMenu.classList.contains("inactive");
+    let isDesktopMenuOpen = !desktopMenu.classList.add("inactive");
+    if(isMobileMenuOpen){
+        mobileMenu.classList.add("inactive");
+    }
+    if(isDesktopMenuOpen){
+        desktopMenu.classList.add("inactive");
+    }
+    
+    shoppingCart.classList.toggle("inactive");
 }
